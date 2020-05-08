042fa4ef6990bcb024dbf3069f8f79b30b4653ef
aljaz <aljazsustar99@gmail.com>
2020-05-08 19:34:11 +0200

    added some wuality of life changes

--- hoops/views.py
+++ hoops/views.py
@@ -139,7 +139,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('/')
+            return redirect('hoops:index')
 
     form = UserCreationForm
     return render(request, 'auth/register.html', {'form': form})
